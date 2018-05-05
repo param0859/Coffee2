1a37a25766994d7fdb62fd86ebc6cb886a872dd0
Param <param0859@gmail.com>
2018-05-05 16:00:53 -0700

    updated sql

--- SQL/Create.sql
+++ SQL/Create.sql
@@ -1,6 +1,6 @@
-DROP USER 'webstudent'@'localhost';
+-- DROP USER 'cmpe'@'localhost';
 FLUSH PRIVILEGES;
-CREATE USER 'webstudent'@'localhost' IDENTIFIED BY 'webstudent';
+CREATE USER 'cmpe'@'localhost' IDENTIFIED BY 'cmpe';
 
 
-GRANT ALL PRIVILEGES ON * . * TO 'webstudent'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'cmpe'@'localhost';
